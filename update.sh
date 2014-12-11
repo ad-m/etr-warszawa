bd8b0f1363b555bc445e00759914023d87941066
Adam Dobrawy <naczelnik@jawnosc.tk>
2014-12-11 12:17:13 +0100

    Update update.sh to remove hard links usage

--- update.sh
+++ update.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 rev=$(date +"%Y-%M-%d %H:%m");
-python etr-warszawa-grab.py >> "revs/$rev.html";
-ln "revs/$rev.html" "revs/current.html";
-
+python etr-warszawa-grab.py | tee "revs/$rev.html" > "revs/current.html";
 git add "revs/$rev.html" "revs/current.html";
 git commit -m "Added rev $rev" "revs/$rev.html" "revs/current.html";
 git push origin;
