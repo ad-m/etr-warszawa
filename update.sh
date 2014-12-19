2c83deebd6317f23749daad43b21bfb9b5345832
Adam Dobrawy <naczelnik@jawnosc.tk>
2014-12-20 00:12:45 +0100

    Added index.html + index generator

--- update.sh
+++ update.sh
@@ -1,6 +1,20 @@
-#!/bin/sh
-rev=$(date +"%Y-%m-%d %T");
-python etr-warszawa-grab.py | tee "revs/$rev.html" > "revs/current.html";
-git add "revs/$rev.html" "revs/current.html";
-git commit -m "Added rev $rev" "revs/$rev.html" "revs/current.html";
-git push origin;
+#!/bin/bash
+function update_revs {
+	rev=$(date +"%Y-%m-%d %T");
+	python etr-warszawa-grab.py | tee "revs/$rev.html" > "revs/current.html";
+	git add "revs/$rev.html" "revs/current.html";
+	git commit -m "Added rev $rev" "revs/$rev.html" "revs/current.html";
+	git push origin;
+};
+function update_index {
+	(cat page/head.html; 
+	find revs -type f | while read line; do 
+		echo "<a href='$line'>`basename "$line"`</a>";
+	done;
+	cat page/footer.html;) > "index.html";
+};
+function update {
+	update_revs;
+	update_index;
+}
+update;
