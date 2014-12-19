558094bc1bbe0d2ff671417d374f17059b31a5e9
Adam Dobrawy <naczelnik@jawnosc.tk>
2014-12-19 21:55:14 +0100

    Fix date pattern in update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-rev=$(date +"%Y-%M-%d %H:%m");
+rev=$(date +"%Y-%m-%d %T");
 python etr-warszawa-grab.py | tee "revs/$rev.html" > "revs/current.html";
 git add "revs/$rev.html" "revs/current.html";
 git commit -m "Added rev $rev" "revs/$rev.html" "revs/current.html";
