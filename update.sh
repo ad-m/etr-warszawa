8c8b88d4cc074608057fc4eb2bff88eedb9df755
Adam Dobrawy <naczelnik@jawnosc.tk>
2014-12-29 03:19:56 +0100

    Commit updated index.html and sitemap.xml

--- update.sh
+++ update.sh
@@ -5,7 +5,6 @@ function update_revs {
 	(cat page/head.html; python etr-warszawa-grab.py; cat page/footer.html;) | tee "revs/$rev.html" > "revs/current.html";
 	git add "revs/$rev.html" "revs/current.html";
 	git commit -m "Added rev $rev" "revs/$rev.html" "revs/current.html";
-	git push origin;
 };
 function update_index {
 	(cat page/head.html; echo "<ul>";
@@ -14,6 +13,9 @@ function update_index {
 	done;
 	echo "</ul>";
 	cat page/footer.html;) > "index.html";
+	git add "index.html";
+	git commit -m "Update index" "index.html";
+
 };
 function generate_sitemap {
 	echo '<urlset>'
@@ -24,10 +26,13 @@ function generate_sitemap {
 }
 function update_sitemap {
 	generate_sitemap > sitemap.xml;
+	git add "sitemap.xml";
+	git commit -m "Update sitemap" "sitemap.xml";
 };
 function update {
 	update_revs;
 	update_index;
 	update_sitemap;
+	git push origin;
 }
 update;
