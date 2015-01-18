6a210faeaaa1f4a2e818547e5ca0dd894dd1171b
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-01-18 20:55:27 +0100

    Pass more data in POST, grab all rows (not only today)

--- etr-warszawa-grab.py
+++ etr-warszawa-grab.py
@@ -5,5 +5,14 @@ import sys
 import requests
 from BeautifulSoup import BeautifulSoup
 # sys.stdout = codecs.getwriter('utf8')(sys.stdout)
-soup = BeautifulSoup(requests.post('http://www.warszawa.wsa.gov.pl/183/elektroniczny-terminarz-rozpraw.html',data={'data_posiedzenia':''}).text)
+data = {'wydzial_orzeczniczy':'---',
+'symbol':'',
+'sygnatura':'',
+'sortowanie':'3',
+'sala_rozpraw':'---',
+'opis':'',
+'guzik':'Filtruj / Sortuj',
+'data_posiedzenia':'',
+'act':'szukaj',}
+soup = BeautifulSoup(requests.post('http://www.warszawa.wsa.gov.pl/183/elektroniczny-terminarz-rozpraw.html',data=data).text)
 print soup.find('table').prettify()
